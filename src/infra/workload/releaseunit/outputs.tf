58f99c607a003cc044b87396e164031088eba866
demo-cfg8 <134722306+demo-cfg8@users.noreply.github.com>
2023-06-03 21:18:31 -0300

    Initialize feature/k8gb-1

---
+++ src/infra/workload/releaseunit/outputs.tf
@@ -0,0 +1,24 @@
+# Build array of objects with properties of each stamp
+output "stamp_properties" {
+  value = [for instance in module.stamp : {
+    location                       = instance.location
+    resource_group_name            = instance.resource_group_name
+    key_vault_name                 = instance.key_vault_name
+    aks_cluster_id                 = instance.aks_cluster_id
+    aks_cluster_name               = instance.aks_cluster_name
+    aks_cluster_ingress_fqdn       = instance.aks_ingress_fqdn
+    aks_cluster_ingress_ip_address = instance.aks_ingress_publicip_address
+    public_storage_account_name    = instance.public_storage_account_name
+    storage_web_host               = instance.public_storage_static_web_host
+  }]
+}
+
+# Output the prefix that was used for all the resources
+output "prefix" {
+  value = local.prefix
+}
+
+output "api_key" {
+  value     = random_password.api_key.result
+  sensitive = true
+}
